f2db51b1b04d7d3974fd938660897a47f8edc846
DrewRick <andrekulik.work@gmail.com>
2021-03-02 20:38:08 +0200

    fix

--- presentation/src/main/java/com/drewrick/movieapp/activity/MainActivity.kt
+++ presentation/src/main/java/com/drewrick/movieapp/activity/MainActivity.kt
@@ -1,27 +1,40 @@
 package com.drewrick.movieapp.activity
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.drewrick.domain.service.MovieService
+import com.drewrick.domain.service.MovieServiceImp
+import com.drewrick.movieapp.App
 import com.drewrick.movieapp.R
 import com.drewrick.movieapp.databinding.ActivityMainBinding
 import com.drewrick.movieapp.fragment.FavouritesFragment
 import com.drewrick.movieapp.fragment.FilmsFragment
 import com.google.android.material.tabs.TabLayoutMediator
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    @Inject
+    lateinit var service: MovieService
+
+    private val compositeDisposable: CompositeDisposable = CompositeDisposable()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(
             this, R.layout.activity_main
         )
+
+        (applicationContext as App).appComponent.inject(this)
 
         val pagerAdapter = PagerAdapter(this)
 
@@ -38,6 +51,26 @@ class MainActivity : AppCompatActivity() {
             }
         }.attach()
 
+
+        //For test only
+        compositeDisposable.add(
+            service
+                .getMovies()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                    {
+                        Log.d("TAG", it.toString())
+                    },
+                    {
+                        Log.d("TAG", it.message.toString())
+                    })
+        )
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        compositeDisposable.dispose()
     }
 
     class PagerAdapter(fragmentActivity: FragmentActivity) :
--- presentation/src/main/java/com/drewrick/movieapp/di/AppComponent.kt
+++ presentation/src/main/java/com/drewrick/movieapp/di/AppComponent.kt
@@ -1,12 +1,11 @@
 package com.drewrick.movieapp.di
 
-import androidx.lifecycle.ViewModel
+import com.drewrick.movieapp.activity.MainActivity
 import dagger.Component
 import javax.inject.Singleton
 
 @Singleton
 @Component(modules = [NetworkModule::class, RepositoryModule::class, ServiceModule::class, AppModule::class])
 interface AppComponent {
-
-    fun inject(viewModel: ViewModel)
+    fun inject(activity: MainActivity)
 }
